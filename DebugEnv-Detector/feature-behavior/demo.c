38a687bd6ecf4997f209641091fd9699d24543c2
QGrain <zhiyuzhang1999@163.com>
2021-10-26 16:42:38 +0800

    update demo

--- DebugEnv-Detector/feature-behavior/demo.c
+++ DebugEnv-Detector/feature-behavior/demo.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 
-BOOL SearchCC(DWORD dwAddr, DWORD dwCodeSize)
+#ifdef WINVER
+// #define _X86_
+#include <windows.h>
+#include <winnt.h>
+
+BOOL Search0xCC(DWORD dwAddr, DWORD dwCodeSize)
 {
     UINT32 i = 0, cnt = 0;
     UINT8 *p = (UINT8 *)dwAddr;
@@ -21,7 +25,7 @@ BOOL SearchCC(DWORD dwAddr, DWORD dwCodeSize)
 }
 
 
-BOOL CheckDebug()  
+BOOL CheckINT3()  
 {  
     PIMAGE_DOS_HEADER pDosHeader;  
     PIMAGE_NT_HEADERS32 pNtHeaders;  
@@ -32,51 +36,127 @@ BOOL CheckDebug()
     pSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD)pNtHeaders + sizeof(pNtHeaders->Signature) + sizeof(IMAGE_FILE_HEADER) +   
                      (WORD)pNtHeaders->FileHeader.SizeOfOptionalHeader);  
     DWORD dwAddr = pSectionHeader->VirtualAddress + dwBaseImage;   
-    DWORD dwCodeSize = pSectionHeader->SizeOfRawData;      
+    // DWORD dwCodeSize = pSectionHeader->SizeOfRawData;      
     BOOL Found = FALSE;  
-    printf("dwAddr=%x, dwCodeSize=%d\n", dwAddr, dwCodeSize);
 
     PIMAGE_OPTIONAL_HEADER pOpHeader = (PIMAGE_OPTIONAL_HEADER)((DWORD)pNtHeaders + sizeof(pNtHeaders->Signature) + sizeof(IMAGE_FILE_HEADER));
     DWORD dwBaseOfCode = pOpHeader->BaseOfCode;
+    DWORD dwCodeSize = pOpHeader->SizeOfCode;
     DWORD dwImageBase = pOpHeader->ImageBase;
+
+    printf("dwAddr=%x, dwCodeSize=%d\n", dwAddr, dwCodeSize);
     printf("dwBaseOfCode=%x, dwImageBase=%x\n", dwBaseOfCode, dwImageBase);
 
-    Found = SearchCC(dwAddr, dwCodeSize);
-    // __asm__ ( "cld\n\t"               
-    //     "mov     %edi,dwAddr\n\t"
-    //     "mov     %ecx,dwCodeSize\n\t"
-    //     "mov     %al,CCh\n\t"
-    //     "repne   scasb\n\t"
-    //     "jnz     NotFound\n\t"
-    //     "mov Found,1\n"
-    // "NotFound:\n" );
+    Found = Search0xCC(dwAddr, dwCodeSize);
+    // __asm__ __volatile__ ( "cld\n\t"               
+    //         "movl     %1, %%edi\n\t"
+    //         "movl     %2, %%ecx\n\t"
+    //         "movb     $0xCC, %%al\n\t"
+    //         "repne    scasb\n\t"
+    //         "jnz      NotFound\n\t"
+    //         "mov      $0x1, %0\n"
+    //         "NotFound:\n"
+    //         : "=g"(Found)
+    //         : "g"(dwAddr), "g"(dwCodeSize)
+    //         : "memory", "%edi", "%ecx", "%al"
+    // );
+
     return Found;  
 }
 
 
-void TestAsm()
+BOOL CheckDebugReg()
 {
-    int foo = 10, bar = 15;
-    __asm__ __volatile__ ( "addl %%ebx, %%eax"
-                                       : "=a"(foo)
-                                       : "a"(foo), "b"(bar)
-                                     );
- 
-    printf("foo+bar=%d\n", foo);
-    return;
+    CONTEXT context;    
+    HANDLE hThread = GetCurrentThread();    
+    context.ContextFlags = CONTEXT_DEBUG_REGISTERS;    
+    GetThreadContext(hThread, &context);    
+    if (context.Dr0 != 0 || context.Dr1 != 0 || context.Dr2 != 0 || context.Dr3!=0) {    
+        return TRUE;    
+    }    
+    return FALSE;
 }
+
+
+BOOL CheckExecTime()  
+{
+    int cnt = 0x0fff;
+    DWORD time1, time2;
+    BOOL CK_RESULT = FALSE;
+
+
+    // first judge, by rdtsc
+    __asm__ __volatile__ ( "rdtsc\n\t"
+        "movl %%eax, %0\n\t"
+        :"=g"(time1)
+        :
+        :"memory", "%eax"
+    );
+
+    while(--cnt);
+
+    __asm__ __volatile__ ( "rdtsc\n\t"
+        "movl %%eax, %0\n\t"
+        :"=g"(time2)
+        :
+        :"memory", "%eax"
+    );
+
+    // printf("time1 = %ld, time2 = %ld\n", time1, time2);
+    printf("time diff1 = %d\n", time2-time1);
+    CK_RESULT = (BOOL)((time2-time1) > 0xffff);
+
+    // second judge, by GetTickCount()
+    time1 = GetTickCount();
+    __asm__ __volatile__ ( 
+        "movl     $0xa, %%ecx\n\t"
+        "movl     $0x6, %%edx\n\t"
+        "movl     $0xa, %%ecx\n\t"
+    ::
+    :"%eax", "%ecx", "%edx"
+    );
+
+    time2 = GetTickCount();
+    printf("time1 = %ld, time2 = %ld\n", time1, time2);
+    printf("time diff2 = %d\n", time2-time1);
+    CK_RESULT = CK_RESULT && ((time2-time1) > 0x1A);
+
+    return CK_RESULT;
+}
+
+
+
+
+
+#else
+BOOL CheckINT3()
+{
+    return FALSE;
+}
+#endif
+
 
 int main(void)
 {
     BOOL stats = FALSE;
     char c;
-    stats = CheckDebug();
-    if(stats == TRUE)
-        printf("You are being debuged, stats = %d\n", stats);
+    if(CheckINT3() == TRUE)
+        // printf("INT3 detected, you are being debuged\n");
+        printf("[*] %-50s%10s\n","INT3 check","[ BAD ]");
     else
-        printf("Nothing detected\n");
+        printf("[*] %-50s%10s\n","INT3 check","[ GOOD ]");
 
-    printf("wait for user input: ");
+    if(CheckDebugReg() == TRUE)
+        printf("[*] %-50s%10s\n","Debug Registers check","[ BAD ]");
+    else
+        printf("[*] %-50s%10s\n","Debug Registers check","[ GOOD ]");
+
+    if(CheckExecTime() == TRUE)
+        printf("[*] %-50s%10s\n","Execution time check","[ BAD ]");
+    else
+        printf("[*] %-50s%10s\n","Execution time check","[ GOOD ]");
+
+    printf("Paused, waiting for user input: ");
     scanf("%c", &c);
     return 0;
 }
